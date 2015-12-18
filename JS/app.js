a879880453bbc9870756da66f1bf934e66bae2cb
simplonco <barbosa.gomis@gmail.com>
2015-12-18 11:25:25 +0100

    Modification....

---
+++ JS/app.js
@@ -0,0 +1,6 @@
+$.getJSON('http://vps227573.ovh.net/u--666.json', function( data ){
+	$('#profil').appendTo('first_name + " " + last_name'), 
+
+	// j'ai lu, relu, plursieurs fois, traduits en français, 
+	//est ce que tu as un élément qui s appele data dans ton html?
+});
